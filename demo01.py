af01d73cfb260dfae9e81e81b37c215ab0353a99
inspiration <a18744000178@qq.com>
2022-01-07 22:50:10 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,25 @@
+# print("hello world!")
+# print(2333)   # 整数
+# print(2.333)  # 小数
+# print(True)   # 布尔值
+# print(())     # 元组
+# print({})     # 数组
+# print({})     # 字典 
+# """
+# 锄禾日当午
+# 汗滴禾下土
+# print("哈哈",2333,2.333)
+# print("哈哈哈"*100)
+# print(1+2)
+# print(2>3) 
+# # 变量
+# # 赋值
+# a="张三" #把张三这个值赋值给了名字叫a的这个变量
+# print(a)
+# c=type(2333)
+# print(c) 
+a = input("请输入: ")
+b = input("请输入: ")
+print("两段字符串的长度是: ",len(a)+len(b))
+
+
